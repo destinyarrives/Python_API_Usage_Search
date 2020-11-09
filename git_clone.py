05a5e5a4ead8cf8795a6df313de3e058f74ed401
TH <hao.teng.2016@smu.edu.sg>
2020-11-09 11:21:38 +0800

    Finalised git_clone.py
    Script now correctly does the following:
    1. opens projects.csv file and generates a list of github urls to download from
    2. opens folder to clone repos into
    3. clones all repos from list of urls (from step 1) into folder (from step 2)

--- git_clone.py
+++ git_clone.py
@@ -1,16 +1,29 @@
-from git import Repo
+import git
+from pathlib import Path
 import pandas as pd
 
 def generate_list_of_urls_to_download(csv_file):
     """
     input: output file from main.py
-    output: list of urls that can be fed into download_raw_file function
+    output: list of urls that can be fed into download_raw_file function 
+            eg - "https://github.com/Project-MONAI/MONAI"
     """
     #results_file = codecs.open("sklearn-cluster-KMeans_October-21-2020_1426PM.txt", "r", encoding="utf-8")
 
     df = pd.read_csv(csv_file)
     df = df[df["Final Label"] == "Y"]
+    df["GitHub Repo"] = "https://github.com/" + df["GitHub Repo"]
     list_of_urls = df["GitHub Repo"].tolist()
     return list_of_urls
 
-generate_list_of_urls_to_download("projects.csv")
+urls = generate_list_of_urls_to_download("projects.csv")
+
+# creates folder to contain repos, if it doesn't already exist
+p = Path.cwd()/"engineered"
+try:
+    Path(p).mkdir()
+except:
+    print("Folder already exists, moving on to the next step...")
+
+for url in urls:
+    git.Git(p).clone(url)
