406fe22a8d3292c1785a0aedc8355296e21bf348
TH <hao.teng.2016@smu.edu.sg>
2020-11-17 16:55:08 +0800

    analysis.py now outputs summary of counts to csv file

--- analysis.py
+++ analysis.py
@@ -1,11 +1,24 @@
 from pathlib import Path
+import os
+import pandas as pd
+
+PYTHON_FILES_TOTAL, FILES_CONTAINING_API_TOTAL, API_USAGE_TOTAL = 0, 0, 0
+APISTATS = {}
 
 def extract():
-    stats_of_apis = {}
-    python_files_total, files_containing_api_total, api_usage_total = 0, 0, 0
+    try:
+        global PYTHON_FILES_TOTAL
+        global FILES_CONTAINING_API_TOTAL
+        global API_USAGE_TOTAL
+        global APISTATS
+    except:
+        pass
+
     p = Path.cwd()/"result_summaries"
     for subdir, dirs, files in os.walk(p):
         for filename in files:
+            if filename.split(".")[-1] != "txt":
+                continue
             with open(str(p) + os.sep + filename, "r") as f:
                 result = f.read()
                 result = result.split("***Python files evaluated in total: ")[1]
@@ -13,7 +26,14 @@ def extract():
                 files, result = result.split("\n***Total API usage count: ")
                 api, result = result.split("\n\n***Time taken: ")
 
-                python_files_total += python
-                files_containing_api_total += files
-                api_usage_total += api
-                stats_of_apis[]
+                PYTHON_FILES_TOTAL += int(python)
+                FILES_CONTAINING_API_TOTAL += int(files)
+                API_USAGE_TOTAL += int(api)
+
+                api_name = filename.split("_November")[0]
+                APISTATS[api_name] = (python, files, api)
+
+if __name__ == "__main__":
+    extract()
+    df = pd.DataFrame.from_dict(APISTATS, orient = "index", columns = ["py files evaluated", "file count", "api count"])
+    df.to_csv("summary_results.csv")
