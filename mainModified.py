46f119341374ed78dc59d38cc8b5aedc512017e9
TH <hao.teng.2016@smu.edu.sg>
2021-01-12 16:33:41 +0800

    Added code to main for cheap search and indexing

--- mainModified.py
+++ mainModified.py
@@ -18,7 +18,20 @@ FORMATTED_QUERY_KEYS = []
 PYTHON_FILEPATHS = utils.get_all_py_files("/media/haoteng/python")
 APIS = process_list_of_apis("data/py_functions_processed_short.txt")
 
-def processFunctionModified(result):
+def cheap_search(api, codefile):
+    with open(codefile, "r") as cf:
+        code = cf.read()
+        return code.find(api)
+
+def build_index():
+    index = {}
+    for api in APIS:
+        index[api] = []
+        for pyfile in PYTHON_FILEPATHS:
+            if cheap_search(api, pyfile) != -1:
+                index[api].append(pyfile)
+
+def processFunction(result):
     file_count, api_instance_count = 0, 0
     try:
         global WRITE_QUEUE
@@ -119,7 +132,7 @@ def main(library, api_query):
     # if an api mention is detected in file f, a copy of f will be saved in ../result_snippets/<api query>/<owner--project>/
     for f in PYTHON_FILEPATHS:
         try:
-            total_file_count, total_api_instance_count = processFunctionModified(f)
+            total_file_count, total_api_instance_count = processFunction(f)
             tfc += total_file_count
             tapic += total_api_instance_count
         except:
@@ -152,6 +165,7 @@ if __name__ == "__main__":
     # torch_apis = [("PyTorch", "is_tensor")]
     for l, q in APIS:
         print(f"Querying for {q}...")
+        #TODO implement cheap search
         main(l, q)
         with WRITE_QUEUE.mutex:
             WRITE_QUEUE.queue.clear()
