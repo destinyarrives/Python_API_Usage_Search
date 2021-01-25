13d39631e4086bd4dc9a7e76e75157e27632daec
TH <hao.teng.2016@smu.edu.sg>
2021-01-25 11:31:31 +0800

    updating build index to redo library level indexing

--- data/scripts/build_index.py
+++ data/scripts/build_index.py
@@ -2,7 +2,28 @@ import ahocorasick, json
 import numpy as np
 import pandas as pd
 
+def acsearch_library_level(haystacks, needles):
+    output = {}
+    for lib in haystacks:
+        output[lib] = []
+
+    A = ahocorasick.Automaton()
+    for idx, key in enumerate(needles):
+        A.add_word(key, (idx, key))
+    A.make_automaton()
+
+    for f in haystacks:
+        with open(f, "r") as haystack:
+            haystack = haystack.read()
+            for end_index, (insert_order, original_value) in A.iter(haystack):
+                if f not in output[original_value]:
+                    output[original_value].append(f)
+
+    return output
+
 def acsearch(haystacks, needles):
+    output = {}
+
     for k, v in haystacks.items():
         print(f"evaluating {k}")
         A = ahocorasick.Automaton()
@@ -16,6 +37,8 @@ def acsearch(haystacks, needles):
                 for end_index, (insert_order, original_value) in A.iter(code):
                     if haystack not in output[k][original_value]:
                         output[k][original_value].append(haystack)
+    
+    return output
 
 # with open("data/py_libraries_processed.txt") as lf:
 #     libraries = lf.read().split("\n")[:-1]
@@ -58,7 +81,15 @@ def match_fqn(fdict, lib, fun):
             return (".".join(item))
 
 if __name__ == '__main__':
-    with open("final_search_v2.json") as outfile:
-        output = clean_final_json()
-        json.dump(output, outfile, indent = 4)
+    # with open("final_search_v2.json") as outfile:
+    #     output = clean_final_json()
+    #     json.dump(output, outfile, indent = 4)
+
+    with open("../verified_python_files.txt", "r") as python_files:
+        python_files = python_files.read().split("\n")
+    with open("../py_libraries_processed.txt", "r") as python_libraries:
+        python_libraries = python_libraries.read().split("\n")
+    acsearch_library_level(python_files, python_libraries)
+
+    
 
