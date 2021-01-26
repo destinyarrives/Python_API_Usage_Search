f2e19d65e6d9fc6b26b49f5370bf4941486a87dc
TH <hao.teng.2016@smu.edu.sg>
2021-01-26 19:46:50 +0800

    Update card_sort.py

--- card_sort.py
+++ card_sort.py
@@ -3,22 +3,40 @@ import os, sys
 from shutil import copy2 
 from random import sample
 
-result = {}
-new_location = Path.cwd()/"manual_analysis"
-new_location.mkdir(exist_ok = True)
-input_file = sys.argv[1]
-with open(str(Path.cwd()) + "/result_summaries/" + input_file, "r") as f:
-    contents = f.read()
-    contents = contents.split("----------------")[1:]
-    for item in sample(contents, 100):
-        filepath = item.partition("File path: ")[2].partition("\nAPI Invocation")[0]
-        filename1 = filepath.partition("--")[2].partition("/")[0]
-        filename2 = filepath.split("/")[-1]
+def find_files_from_result(c):
+    result = {}
+    c = c.split("----------------")[1:]
+    if len(c) > 10:
+        c = sample(c, 10)
+    for item in c: 
+        fpath = item.partition("File path: ")[2].partition("\nAPI Invocation")[0]
+        filename1 = fpath.partition("--")[2].partition("/")[0]
+        filename2 = fpath.split("/")[-1]
         filename = filename1 + "--" + filename2
-        result[filename] = filepath
+        result[filename] = fpath
+    return result
 
-i = 0
-with open("manualAnalysis.txt", "w") as outfile:
-    for fn, fp in result.items():
-        copy2(fp, new_location/fn)    
-        outfile.write(fp + "\n\n")
+if __name__ == '__main__':
+    new_location = Path.cwd()/"manual_analysis"
+    new_location.mkdir(exist_ok = True)
+    DIRECTORY = "data/testdata/temp_results/1_260121"
+
+    all_apis = []
+    for subdir, dirs, files in os.walk(DIRECTORY): 
+        for filename in files:
+            filepath = subdir + os.sep + filename
+            all_apis.append(filepath)
+    
+    for api in sample(all_apis, 50):
+        print(f"choosing {api}")
+        ct = 0 #TODO possibly automate this for average accuracy computation using pandas?
+
+        folder = api.split("/")[4].split(".")[0]
+        with open(api, "r") as f:
+            contents = f.read()
+            output_dict = find_files_from_result(contents)
+        with open(f"manualAnalysis_{ct}.txt", "w") as outfile:
+            for fn, fp in output_dict.items():
+                copy2(fp, new_location/folder/fn) #! need to further organise by api 
+                outfile.write(fp + "\n\n")
+        ct += 1
